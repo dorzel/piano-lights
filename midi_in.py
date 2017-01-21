fea18ff413ffa661f62740031ad567f081887a94
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-20 20:49:52 -0700

    threaded watcher, non working

--- midi_in.py
+++ midi_in.py
@@ -1,6 +1,8 @@
 import mido
 from neopixel import *
 from math import ceil
+from threading import Thread
+from time import sleep
 
 pixel_strip = Adafruit_NeoPixel(120, 18, 800000)
 pixel_strip.begin()
@@ -16,6 +18,19 @@ class PixelWatcher:
     def __init__(self, strip):
         self._strip = strip
         self._pixels = {}
+        self._watch_thread = None
+        self.active = True
+
+    def start(self):
+        # spawn a new thread to process effects
+        print('starting watch thread...')
+        self._watch_thread = Thread(target=self.run_all_effects)
+        print('watch thread: {}'.format(self._watch_thread))
+        self._watch_thread.start()
+
+    def stop(self):
+        self.active = False
+        self._watch_thread.join()
 
     def watch_pixel(self, pixel_num, initial_color):
         # sets a pixel to be watched. initial_color must be in [r,g,b] form.
@@ -37,17 +52,23 @@ class PixelWatcher:
     def _run_effect(pixel_num):
         result = self._pixels[pixel_num]['effect_func'](self._pixels['current_color'])
         if result:
+            # set the color after the effect function has modified the color values
             self._strip.setPixelColorRGB(pixel_num, self._pixels['current_color'][0],
                                                     self._pixels['current_color'][1],
-                                                    self._pixels['current_color'][2]])
+                                                    self._pixels['current_color'][2])
         else:
             # effect has signaled that it is done
             self._remove_pixel(pixel_num)
 
     def run_all_effects(self):
-        for pixel in self._pixels.keys():
-            self._run_effect(pixel)
-        self._strip.show()
+        print('in run effects')
+        while self.active:
+            print('in run effect while loop')
+            for pixel in self._pixels.keys():
+                self._run_effect(pixel)
+            # better to just call one show() after all effects have been run
+            self._strip.show()
+            sleep(0.1)
 
 watcher = PixelWatcher(pixel_strip)
 
@@ -66,6 +87,7 @@ def set_pixel(note, velocity):
     if velocity:
         # note was pressed down
         try:
+            print('put watch on pixel {}'.format(note))
             watcher.watch_pixel(note, color_from_velocity(velocity))
         except Exception as e:
             print(e)
@@ -76,16 +98,17 @@ def set_pixel(note, velocity):
 # velocity: if key was pressed down, an int related to amount of force on
 # press, if key was released, velocity is 0.
 set_blank()
-while True:
-    try:
-        for msg in mido.open_input(mido.get_input_names()[0]):
-            if msg.type != 'clock':
-                if msg.type != 'control_change':
-                    set_pixel(msg.note, msg.velocity)
-                else:
-                    # floor pedal was pressed
-                    pass
-    except KeyboardInterrupt:
-        set_blank()
-        print('done.')
-        break
+watcher.start()
+try:
+    for msg in mido.open_input(mido.get_input_names()[0]):
+        print('in midi loop')
+        if msg.type != 'clock':
+            if msg.type != 'control_change':
+                set_pixel(msg.note, msg.velocity)
+            else:
+                # floor pedal was pressed
+                pass    
+except KeyboardInterrupt:
+    set_blank()
+    watcher.stop()
+    print('done.')
