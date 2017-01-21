032c23af5f480e885569b8c93331ee4846059cb9
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-20 20:20:39 -0700

    pixel watcher implementation before threading

--- midi_in.py
+++ midi_in.py
@@ -21,21 +21,35 @@ class PixelWatcher:
         # sets a pixel to be watched. initial_color must be in [r,g,b] form.
         if not pixel_num in self._pixels:
             self._pixels[pixel_num] = {'current_color': initial_color}
+        else:
+            # when pressed again, reset the color
+            self._pixels[pixel_num]['current_color'] = initial_color
 
     def _remove_pixel(self, pixel_num):
         self._pixels.pop(pixel_num)
 
     def add_effect(pixel_num, effect_func):
         # effect_func must take in an [r,g,b] list and perform operations in place
-        # on those values
+        # on those values. It also must return None when the effect is finished,
+        # which means that it must monitor its own "doneness"
         self._pixels[pixel_num]['effect_func'] = effect_func
 
     def _run_effect(pixel_num):
-        self._pixels[pixel_num]['effect_func'](self._pixels['current_color'])
+        result = self._pixels[pixel_num]['effect_func'](self._pixels['current_color'])
+        if result:
+            self._strip.setPixelColorRGB(pixel_num, self._pixels['current_color'][0],
+                                                    self._pixels['current_color'][1],
+                                                    self._pixels['current_color'][2]])
+        else:
+            # effect has signaled that it is done
+            self._remove_pixel(pixel_num)
 
     def run_all_effects(self):
         for pixel in self._pixels.keys():
             self._run_effect(pixel)
+        self._strip.show()
+
+watcher = PixelWatcher(pixel_strip)
 
 
 def set_blank():
@@ -44,21 +58,17 @@ def set_blank():
     pixel_strip.show()
 
 def color_from_velocity(velocity):
-    return  Color(ceil(scale_factor*velocity*base_color[0]),
-                 ceil(scale_factor*velocity*base_color[1]),
-                 ceil(scale_factor*velocity*base_color[2]))
+    return  [ceil(scale_factor*velocity*base_color[0]),
+             ceil(scale_factor*velocity*base_color[1]),
+             ceil(scale_factor*velocity*base_color[2])]
 
 def set_pixel(note, velocity):
     if velocity:
         # note was pressed down
         try:
-            pixel_strip.setPixelColor(note, color_from_velocity(velocity))
-            pixel_strip.show()
+            watcher.watch_pixel(note, color_from_velocity(velocity))
         except Exception as e:
             print(e)
-    else:
-        pixel_strip.setPixelColorRGB(note, 0, 0, 0)
-        pixel_strip.show()
 
 
 # get midi input from piano, incoming data is a msg with attributes:
@@ -76,5 +86,6 @@ while True:
                     # floor pedal was pressed
                     pass
     except KeyboardInterrupt:
+        set_blank()
         print('done.')
         break
