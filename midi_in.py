a9b0556f5fc3a0b5a908f38227d5e9940f28c660
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-21 18:46:31 -0700

    working pixel effects

--- midi_in.py
+++ midi_in.py
@@ -1,6 +1,6 @@
+from __future__ import division, print_function
 import mido
 from neopixel import *
-from math import ceil
 from threading import Thread
 from time import sleep
 
@@ -10,7 +10,7 @@ pixel_strip.begin()
 # scale_factor is related to the max key velocity, 100.
 # base_color is the base pixel color to be scaled with velocity later
 scale_factor = 1/100
-base_color = (255, 20, 20)
+base_color = (255, 255, 0)
 blank_color = Color(0, 0, 0)
 
 
@@ -23,9 +23,9 @@ class PixelWatcher:
 
     def start(self):
         # spawn a new thread to process effects
-        print('starting watch thread...')
+        #print('starting watch thread...')
         self._watch_thread = Thread(target=self.run_all_effects)
-        print('watch thread: {}'.format(self._watch_thread))
+        #print('watch thread: {}'.format(self._watch_thread))
         self._watch_thread.start()
 
     def stop(self):
@@ -35,44 +35,45 @@ class PixelWatcher:
     def watch_pixel(self, pixel_num, initial_color):
         # sets a pixel to be watched. initial_color must be in [r,g,b] form.
         if not pixel_num in self._pixels:
-            print('put watch on pixel {}'.format(pixel_num))
+            #print('put watch on pixel {} with initial color {}'.format(pixel_num, initial_color))
             self._pixels[pixel_num] = {'current_color': initial_color}
         else:
             # when pressed again, reset the color
-            print('reset color for pixel {}'.format(pixel_num))
+            #print('reset color for pixel {}'.format(pixel_num))
             self._pixels[pixel_num]['current_color'] = initial_color
 
     def _remove_pixel(self, pixel_num):
-        print('removed watch on pixel {}'.format(pixel_num))
+        #print('removed watch on pixel {}'.format(pixel_num))
         self._pixels.pop(pixel_num)
 
-    def add_effect(pixel_num, effect_func):
-        # effect_func must take in an [r,g,b] list and perform operations in place
-        # on those values. It also must return None when the effect is finished,
-        # which means that it must monitor its own "doneness"
+    def add_effect(self, pixel_num, effect_func):
+        # effect_func must take in an [r,g,b] list and return a new list of transformed rgb values.
+        # It also must return None when the effect is finished, which means that it must monitor
+        # its own "doneness"
         self._pixels[pixel_num]['effect_func'] = effect_func
-        print('added effect {} to pixel {}'.format(effect_func, pixel_num))
+        #print('added effect {} to pixel {}'.format(effect_func, pixel_num))
 
-    def _run_effect(pixel_num):
-        result = self._pixels[pixel_num]['effect_func'](self._pixels['current_color'])
+    def _run_effect(self, pixel_num):
+        result = self._pixels[pixel_num]['effect_func'](self._pixels[pixel_num]['current_color'])
         if result:
             # set the color after the effect function has modified the color values
-            self._strip.setPixelColorRGB(pixel_num, self._pixels['current_color'][0],
-                                                    self._pixels['current_color'][1],
-                                                    self._pixels['current_color'][2])
+            self._pixels[pixel_num]['current_color'] = result
+            self._strip.setPixelColorRGB(pixel_num, result[0],
+                                                    result[1],
+                                                    result[2])
         else:
             # effect has signaled that it is done by returning None
             self._remove_pixel(pixel_num)
 
     def run_all_effects(self):
-        print('in run effects')
         while self.active:
-            print('in run effect while loop')
             for pixel_num in list(self._pixels.keys()):
                 self._run_effect(pixel_num)
             # better to just call one show() after all effects have been run
             self._strip.show()
-            sleep(0.1)
+            # this sleep controls how fast all of the effects are done, just need to hand
+            # calibrate it with the effect functions for now.
+            sleep(0.05)
 
 watcher = PixelWatcher(pixel_strip)
 
@@ -83,15 +84,15 @@ def set_blank():
     pixel_strip.show()
 
 def color_from_velocity(velocity):
-    return  [ceil(scale_factor*velocity*base_color[0]),
-             ceil(scale_factor*velocity*base_color[1]),
-             ceil(scale_factor*velocity*base_color[2])]
+    return  [int(scale_factor*velocity*base_color[0]),
+             int(scale_factor*velocity*base_color[1]),
+             int(scale_factor*velocity*base_color[2])]
 
 def reduce_effect(rgb_in):
-    if not all([comp == 0 for comp in rgb_in]):
-        rgb_in = [comp - 1 for comp in rgb_in if comp != 0]
-        return True
+    if not all(comp == 0 for comp in rgb_in):
+        return [comp -10 if comp > 0 and comp - 10 > 0 else 0 for comp in rgb_in]
     else:
+        # all components are 0, ending the effect
         return None
 
 def set_pixel(note, velocity):
@@ -102,6 +103,9 @@ def set_pixel(note, velocity):
             watcher.add_effect(note, reduce_effect)
         except Exception as e:
             print(e)
+    else:
+        # key has been released
+        pass
 
 
 # get midi input from piano, incoming data is a msg with attributes:
@@ -112,7 +116,6 @@ set_blank()
 watcher.start()
 try:
     for msg in mido.open_input(mido.get_input_names()[0]):
-        #print('in midi loop')
         if msg.type != 'clock':
             if msg.type != 'control_change':
                 set_pixel(msg.note, msg.velocity)
